9fa0c99f1430e09cdf11d3808fe81dfac094044a
“ashafin758” <ashafin758@gmail.com>
2022-12-08 10:52:45 +0600

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+route::get('/', [HomeController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+route::get('/redirect', [HomeController::class, 'redirect']);
+
+route::get('/view_catagory', [AdminController::class, 'view_catagory']);
+
+route::post('/add_catagory', [AdminController::class, 'add_catagory']);
+
+route::post('/delete_catagory/{id}', [AdminController::class, 'delete_catagory']);
+
